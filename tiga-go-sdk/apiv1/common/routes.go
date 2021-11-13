b25d7434f443fe357599f973dc6ac5ec067e261f
imulab <david@absurdlab.io>
2021-11-13 13:51:46 +0800

    feat(tiga-go-sdk): Add additional route endpoints

--- tiga-go-sdk/apiv1/common/routes.go
+++ tiga-go-sdk/apiv1/common/routes.go
@@ -14,8 +14,20 @@ func (_ route) AuthorizeURL(baseUrl string) string {
 	return fmt.Sprintf("%s/oauth/authorize", trimTrailingSlash(baseUrl))
 }
 
+func (_ route) ResumeAuthorizeURL(baseUrl string, authorizeSessionId string) string {
+	return fmt.Sprintf("%s/oauth/authorize/session/%s/resume", trimTrailingSlash(baseUrl), authorizeSessionId)
+}
+
 func (_ route) TokenURL(baseUrl string) string {
 	return fmt.Sprintf("%s/oauth/token", trimTrailingSlash(baseUrl))
+}
+
+func (_ route) TokenRevokeURL(baseUrl string) string {
+	return fmt.Sprintf("%s/oauth/token/revoke", trimTrailingSlash(baseUrl))
+}
+
+func (_ route) UserInfoURL(baseUrl string) string {
+	return fmt.Sprintf("%s/oauth/token/userinfo", trimTrailingSlash(baseUrl))
 }
 
 func (_ route) DiscoveryURL(baseUrl string) string {
@@ -24,10 +36,6 @@ func (_ route) DiscoveryURL(baseUrl string) string {
 
 func (_ route) JSONWebKeySetURL(baseUrl string) string {
 	return fmt.Sprintf("%s/.well-known/jwks.json", trimTrailingSlash(baseUrl))
-}
-
-func (_ route) ResumeAuthorizeURL(baseUrl string, authorizeSessionId string) string {
-	return fmt.Sprintf("%s/oauth/authorize/session/%s/resume", trimTrailingSlash(baseUrl), authorizeSessionId)
 }
 
 func trimTrailingSlash(url string) string {
